a3e61bf8362902a4c518f558a7a9caa73ef08190
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-11-26 11:07:31 +0100

    importato FontAwesome + aggiunto footer V.1

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,12 +9,50 @@ export default {
 </script>
 
 <template>
-  <header>
-   
-  </header>
+  <footer>
+    <div class="container">
+      <div class="row justify-content-between align-items-center">
+        <div class="col-xl-2 col-lg-2 col-sm-2 ">
+          <h2>
+            DeliveBoo
+          </h2>
+        </div>
+        <div class="col-xl-2 col-lg-2 col-sm-2">
+          <i class="fa-regular fa-copyright text-light"></i>
+          <span>
+            2024 Deliveroo
+          </span>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
 
+footer {
+
+  background-color: black;
+
+  .container {
+
+    .row {
+
+      height: 50px;
+      .col-xl-2 > *{
+
+        color: white;
+        opacity: 0.8;
+        text-align: center;
+        
+        &:hover {
+
+          cursor: pointer;
+          opacity: 1;
+        }
+      }
+    }
+  }  
+}
 </style>
