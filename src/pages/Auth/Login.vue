913b2089d07676616e4ef61b78a1a9269d6dd725
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-11-27 17:41:21 +0100

    Aggiunte funzioni java + form

--- src/pages/Auth/Login.vue
+++ src/pages/Auth/Login.vue
@@ -2,7 +2,29 @@
 export default {
   data() {
     return { 
-      
+      form: {
+        email: '',
+        password: ''
+      },
+    }
+  },
+  methods: {
+    submitForm() {
+      // Crea un oggetto FormData per inviare i dati (inclusi file)
+      const formData = new FormData();
+      formData.append('email', this.form.email);
+      formData.append('password', this.form.password);
+
+      // Esegui la richiesta POST per registrare l'utente e il ristorante
+      axios.post('http://127.0.0.1:8000/api/auth/login', formData)
+        .then(response => {
+          console.log('Registrazione completata:', response.data);
+          // Puoi reindirizzare l'utente o fare altre azioni
+        })
+        .catch(error => {
+          console.error('Errore durante la registrazione:', error.response.data);
+          console.error('status: ', error.response.status);
+        });
     }
   }
 }
@@ -15,19 +37,21 @@ export default {
         Login
       </div>
       <div class="card-body">
-        <form action="">
+        <form @submit.prevent="submitForm" method="post">
 
           <!-- Email -->
           <div class="mb-3">
-            <label for="EmailInput" class="form-label">Indirizzo Email</label>
-            <input type="email" class="form-control" id="EmailInput" placeholder="" required>
+            <label for="EmailInput" class="form-label">Indirizzo Email  
+              <span class="red_required">*</span> 
+            </label>
+            <input type="email" v-model="form.email" class="form-control" id="EmailInput" placeholder="Inserisci qui la tua Email..." required min="5">
           </div>
 
           <!-- Password -->
           <div class="row g-3 align-items-center mb-4">
             <div class="col-auto">
               <label for="inputPassword6" class="col-form-label">Password</label>
-              <input type="password" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
+              <input type="password" v-model="form.password" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline" placeholder="Inserisci qui la tua Password..." min="8">
             </div>
           </div>
 
