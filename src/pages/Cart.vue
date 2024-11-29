767ec70b9283e70431d002321414aad890c1bde6
lauraoliveri <lauraoliveri2003@gmail.com>
2024-11-29 10:29:05 +0100

    Aggiunta del carrello

---
+++ src/pages/Cart.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from 'axios';
+
+
+</script>
+
+<template>
+  <main>
+    <!-- Carrello Offcanvas -->
+
+    carrelloooo
+  </main>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+
+
+.cart {
+  font-family: "Chewy", system-ui;
+  font-weight: 400;
+  font-style: normal;
+}
+
+</style>
